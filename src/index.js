34339849a74a5b63e8ea6083cb61e780a8aa5d7f
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-06-11 17:51:18 +0700

    Setup leaderboard table layout

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import './style.css';
+
+const dataRank = [
+  {
+    name: 'John',
+    points: Math.floor(Math.random() * 100),
+  },
+  {
+    name: 'Doe',
+    points: Math.floor(Math.random() * 100),
+  },
+  {
+    name: 'Mel',
+    points: Math.floor(Math.random() * 100),
+  },
+  {
+    name: 'Carl',
+    points: Math.floor(Math.random() * 100),
+  },
+];
+
+function loop(data) {
+  return `
+    <tr>
+      <td>${data.name}</td>
+      <td>${data.points}</td>
+    </tr>
+  `;
+}
+
+const table = document.querySelector('#table-body');
+
+table.innerHTML = dataRank.map((data) => loop(data)).join('');
